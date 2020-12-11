df5263470edd4863186656d9bab03d1cd7848287
Luiz Pires <luiz.pires@compasso.com.br>
2020-12-11 05:43:59 -0300

    feat: adicionado saudções boas vindas

--- src/main/intents/saudacoes/saudacoes-mensagem-boas-vindas.js
+++ src/main/intents/saudacoes/saudacoes-mensagem-boas-vindas.js
@@ -1,12 +1,21 @@
 import { Text } from 'dialogflow-fulfillment'
 
-function saudacoesMensagemBoasVindas (agent, body) {
-  const message = `Olá, ${agent.originalRequest.payload.data.from.first_name} ${agent.originalRequest.payload.data.from.last_name}! Sou a Doutora Silvia, uma assistente virtual treinada para tirar suas dúvidas relacionadas ao Coronavírus. 👩\n\n Neste canal, você poderá tirar dúvidas comigo sobre prevenção, contágio, casos no Brasil ou realizar um pré-diagnóstico, por exemplo.\n\n E não se preocupe, pois todos os dados que eu te contar são retirados de fontes seguras que você pode confiar.`
-  agent.add(new Text(message))
-  agent.add(new Text('message'))
+function saudacoesMensagemBoasVindas (agent) {
+  const userExists = data.filter(userId => agent.originalRequest.payload.data.from.id === userId.id).length
 
-  console.log(agent)
-  agent.requestSource = agent.TELEGRAM
+  const menu = '| Prevenção | Contágio | Casos no Brasil | Casos no Brasil | Outras dúvidas |'
+  let message = `Olá, ${agent.originalRequest.payload.data.from.first_name} ${agent.originalRequest.payload.data.from.last_name}! Sou a Doutora Silvia, uma assistente virtual treinada para tirar suas dúvidas relacionadas ao Coronavírus. 👩\n\n Neste canal, você poderá tirar dúvidas comigo sobre prevenção, contágio, casos no Brasil ou realizar um pré-diagnóstico, por exemplo.\n\n E não se preocupe, pois todos os dados que eu te contar são retirados de fontes seguras que você pode confiar.`
+
+  if (userExists === 0) {
+    // data.push({ id: agent.originalRequest.payload.data.from.id })
+    // cadastra o id do usuario no mongo
+  } else {
+    message = `Olá novamente, ${agent.originalRequest.payload.data.from.first_name} ${agent.originalRequest.payload.data.from.last_name} ! Sou uma assistente virtual treinada para tirar suas dúvidas relacionadas ao Coronavírus.️ 👩`
+  }
+
+  agent.add(new Text(message))
+  agent.add(new Text('Sobre qual assunto você quer saber?'))
+  agent.add(menu)
 }
 
 export { saudacoesMensagemBoasVindas }
