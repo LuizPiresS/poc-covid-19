71bed37553c64e87aacf7633e15626e7bdcd2dab
Luiz Pires <luiz.pires@compasso.com.br>
2020-12-12 01:59:27 -0300

    chore: adicionado host remoto para o mongo

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-mongoose.connect('mongodb://localhost/poc-covid-19')
+mongoose.connect('mongodb+srv://lps:qA17TabS72SMiOu2@cluster0.nxdyj.mongodb.net/poc-covid?retryWrites=true&w=majority')
 mongoose.Promise = global.Promise
 
 export default mongoose
