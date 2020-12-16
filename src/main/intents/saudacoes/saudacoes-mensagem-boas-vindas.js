6cc5b73b49dd9225f170c1bef4e58abf9dde109b
Luiz Pires <luiz.pires@compasso.com.br>
2020-12-16 09:36:00 -0300

    fix: corrigido bug do retorno do nome como undefined

--- src/main/intents/saudacoes/saudacoes-mensagem-boas-vindas.js
+++ src/main/intents/saudacoes/saudacoes-mensagem-boas-vindas.js
@@ -3,13 +3,15 @@ import { Suggestion, Text } from 'dialogflow-fulfillment'
 import User from '../../../database/models/user'
 
 async function saudacoesMensagemBoasVindas (agent) {
+  const firstName = agent.originalRequest.payload.data.from.first_name || ''
+  const lastName = agent.originalRequest.payload.data.from.last_name || ''
   const userId = agent.originalRequest.payload.data.from.id
 
-  let message = `Olá, ${agent.originalRequest.payload.data.from.first_name} ${agent.originalRequest.payload.data.from.last_name}! Sou a Doutora Silvia, uma assistente virtual treinada para tirar suas dúvidas relacionadas ao Coronavírus. 👩\n\nNeste canal, você poderá tirar dúvidas comigo sobre prevenção, contágio, casos no Brasil ou realizar um pré-diagnóstico, por exemplo.\n\nE não se preocupe, pois todos os dados que eu te contar são retirados de fontes seguras que você pode confiar.`
+  let message = `Olá, ${firstName} ${lastName}! Sou a Doutora Silvia, uma assistente virtual treinada para tirar suas dúvidas relacionadas ao Coronavírus. 👩\n\nNeste canal, você poderá tirar dúvidas comigo sobre prevenção, contágio, casos no Brasil ou realizar um pré-diagnóstico, por exemplo.\n\nE não se preocupe, pois todos os dados que eu te contar são retirados de fontes seguras que você pode confiar.`
 
   try {
     if (await User.findOne({ userId })) {
-      message = `Olá novamente, ${agent.originalRequest.payload.data.from.first_name} ${agent.originalRequest.payload.data.from.last_name} ! Sou uma assistente virtual treinada para tirar suas dúvidas relacionadas ao Coronavírus.️ 👩`
+      message = `Olá novamente, ${firstName} ${lastName} ! Sou uma assistente virtual treinada para tirar suas dúvidas relacionadas ao Coronavírus.️ 👩`
     } else {
       await User.create({ userId })
     }
