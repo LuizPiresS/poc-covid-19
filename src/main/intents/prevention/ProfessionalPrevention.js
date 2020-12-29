f4e397bf4aae21230846a337100564c14106e9aa
Luiz Pires <luiz.pires@compasso.com.br>
2020-12-29 09:30:35 -0300

    refactor: removendo comentarios desnecessarios

--- src/main/intents/prevention/ProfessionalPrevention.js
+++ src/main/intents/prevention/ProfessionalPrevention.js
@@ -4,15 +4,15 @@ export class ProfessionalPrevention {
   static execute (agent) {
     const mensagem = 'Os profissionais de saúde devem utilizar as medidas de precaução padrão estabelecidas. 👍\n' +
     '\n' +
-    'Ao prestar serviços que atendam casos suspeitos do vírus, é orientado que os profissionais tenham disponibilidade dos seguintes equipamentos de proteção individual:\n' +
+   'Ao prestar serviços que atendam casos suspeitos do vírus, é orientado que os profissionais tenham disponibilidade dos seguintes equipamentos de proteção individual:\n' +
     '\n' +
-    '⚠ Para serviços em ambulatório:\n' +
+   '⚠ Para serviços em ambulatório:\n' +
     'O uso de máscara cirúrgica e luvas descartáveis.\n' +
     '\n' +
     '⚠ Para o atendimento em UPA, Pronto Socorro, UTI e Unidade semi-intensiva: É importante o uso de máscaras padrão N95 ou similar; luvas descartáveis; gorro; capote cirúrgico e óculos de proteção ou protetor facial.\n' +
-    '\n' +
-    '⚠ Além disso, para a realização de procedimentos que gerem aerossolização de secreções respiratórias como intubação, aspiração de vias aéreas ou indução de escarro, deve ser utilizado precaução por aerossóis, com uso de máscara N95.\n' +
-    '😷'
+   '\n' +
+     '⚠ Além disso, para a realização de procedimentos que gerem aerossolização de secreções respiratórias como intubação, aspiração de vias aéreas ou indução de escarro, deve ser utilizado precaução por aerossóis, com uso de máscara N95.\n' +
+   '😷'
 
     agent.add(new Text(mensagem))
 
