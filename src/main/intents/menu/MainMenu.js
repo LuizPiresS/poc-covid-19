a30b88af22b02925abc94246c0e78cc1ee49cf4e
Luiz Pires <luiz.pires@compasso.com.br>
2021-01-04 12:03:59 -0300

    refactor: renomeado menu para show menu

--- src/main/intents/menu/MainMenu.js
+++ src/main/intents/menu/MainMenu.js
@@ -5,17 +5,14 @@ export class MainMenu {
    * Exibe o menu principal
    * @param agent
    * @param message
-   * @param menu (define se o messageMenu será exibido ou não)
+   * @param showMenu (define se o messageMenu será exibido ou não)
    */
-  static execute (agent, message, menu = false) {
+  static execute (agent, message, showMenu = false) {
     const messageMenu = 'Você pode tirar dúvidas comigo sobre\n prevenção, contágio, casos no Brasil ou\n realizar um pré-diagnóstico.'
     agent.add(new Text(message))
-    if (menu) {
-      agent.add(new Text(messageMenu))
-    }
-    agent.add(new Text('Sobre qual assunto você quer saber?'))
+
     agent.add(new Suggestion({
-      title: ' ',
+      title: showMenu ? messageMenu : null,
       reply: 'Prevenção'
     }))
     agent.add(new Suggestion({
@@ -26,5 +23,6 @@ export class MainMenu {
       title: 'Casos no Brasil',
       reply: 'Casos no Brasil'
     }))
+    agent.add(new Text('Sobre qual assunto você quer saber?'))
   }
 }
