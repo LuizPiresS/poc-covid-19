d2eb0a566ee505dfe0ac0ee6b7eb3a5c7f885069
Luiz Pires <luiz.pires@compasso.com.br>
2021-01-05 11:55:50 -0300

    feat: integrando o casos brasil ao Telegram e formatando o texto

--- src/main/intents/cases/CasesBrazil.js
+++ src/main/intents/cases/CasesBrazil.js
@@ -1,30 +1,21 @@
-import axios from 'axios'
-import config from 'config'
 import { Text } from 'dialogflow-fulfillment'
 
-const tokenAPICovid = config.get('App.Auth.tokenAPICovid')
+import { APICasesBrazil } from '../utils/apis/APICasesBrazil'
+
 export class CasesBrazil {
   static async execute (agent) {
-    let placeType = 'city'
-    let city = agent.parameters.location.city
-    const state = agent.parameters.states
+    try {
+      const country = agent.parameters.location.country
 
-    if (state && !city) {
-      placeType = 'state'
-      city = ''
+      const apiCasesBrazil = new APICasesBrazil()
+
+      if (country) {
+        agent.add(new Text(await apiCasesBrazil.getCasesFromCountries(agent)))
+      } else {
+        agent.add(new Text(await apiCasesBrazil.getCasesFromStatesOrCities(agent)))
+      }
+    } catch (error) {
+      console.log(error)
     }
-    const casesCovidData = await axios.get(`https://api.brasil.io/v1/dataset/covid19/caso_full/data/?place_type=${placeType}&city=${city}&state=${state}&is_last=True`,
-      {
-        headers: {
-          Authorization: `Token ${tokenAPICovid}`
-        }
-      })
-      .then(res => res.data)
-      .catch(err => {
-        console.log(err)
-      })
-
-    console.log(casesCovidData.results[0])
-    agent.add(new Text('casos no brasil'))
   }
 }
---
+++ src/main/intents/utils/apis/APICasesBrazil.js
@@ -0,0 +1,67 @@
+import axios from 'axios'
+import config from 'config'
+
+export class APICasesBrazil {
+  /**
+   * Pega os dados dos casos de covid no Brasil por estadado ou Cidade
+   * @param agent
+   * @returns {Promise<AxiosResponse<any> | void>}
+   */
+  async getCasesFromStatesOrCities (agent) {
+    const tokenAPICovid = config.get('App.Auth.tokenAPICovid')
+
+    let placeType = 'city'
+    let city = agent.parameters.location.city
+    const state = agent.parameters.states
+
+    if (state && !city) {
+      placeType = 'state'
+      city = ''
+    }
+
+    return axios.get(`https://api.brasil.io/v1/dataset/covid19/caso_full/data/?place_type=${placeType}&city=${city}&state=${state}&is_last=True`,
+      {
+        headers: {
+          Authorization: `Token ${tokenAPICovid}`
+        }
+      })
+      .then(res => {
+        return `
+Aqui estão os dados mais recentes para ${city} ${state}:
+
+⚠ Total de casos:
+
+- Confirmados: ${res.data.results[0].last_available_confirmed}
+- Mortes: ${res.data.results[0].last_available_deaths}
+
+⚠ Casos no dia de hoje:
+
+- Confirmados: ${res.data.results[0].new_confirmed}
+- Mortes: ${res.data.results[0].new_deaths}
+
+Posso ajudar em algo mais?`
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  async getCasesFromCountries (agent) {
+    return axios.get('https://covid19-brazil-api.now.sh/api/report/v1/brazil')
+      .then(res => {
+        return `Aqui estão os dados mais recentes para o Brasil:
+
+⚠ Total de casos:
+
+- Confirmados: ${res.data.data.confirmed}
+- Recuperados: ${res.data.data.recovered}
+- Mortes: ${res.data.data.deaths}
+
+Posso ajudar em algo mais?
+`
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+}
