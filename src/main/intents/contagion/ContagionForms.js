231bc3ed55d385bcf2f8979d51dab215f3de7af5
Luiz Pires <luiz.pires@compasso.com.br>
2021-01-18 09:40:27 -0300

    fix: resolvido problemas de espaçamento nos paragrafos

--- src/main/intents/contagion/ContagionForms.js
+++ src/main/intents/contagion/ContagionForms.js
@@ -2,20 +2,19 @@ import { Text, Suggestion } from 'dialogflow-fulfillment-helper'
 
 export class ContagionForms {
   static execute (agent) {
-    const mensagem = 'A transmissão do vírus acontece por via\n ' +
-    'respiratória, através de gotículas que se\n ' +
+    const mensagem = 'A transmissão do vírus acontece por via\n' +
+    'respiratória, através de gotículas que se\n' +
     'espalham pelo ar quando uma pessoa que\n' +
-    ' está infectada tosse ou espirra. 💦\n' +
+    'está infectada tosse ou espirra. 💦\n' +
     '\n\n' +
     'Também é possível se contaminar por\n' +
-    ' contato pessoal com as secreções\n' +
-    ' infectadas, como: gotículas de saliva;\n ' +
-    'espirro; tosse; catarro; contato pessoal\n ' +
-    'próximo, como toque ou aperto de mão; e\n ' +
+    'contato pessoal com as secreções\n' +
+    'infectadas, como: gotículas de saliva;\n' +
+    'espirro; tosse; catarro; contato pessoal\n' +
+    'próximo, como toque ou aperto de mão; e\n' +
     'o contato com roupas e objetos\n' +
-    ' contaminados.'
+    'contaminados.'
     agent.add(new Text(mensagem))
-    agent.add(new Text(''))
     agent.add(new Suggestion({
       title: 'Posso ajudar em algo mais? ',
       reply: 'sim'
--- src/main/intents/contagion/IncubationPeriod.js
+++ src/main/intents/contagion/IncubationPeriod.js
@@ -4,7 +4,7 @@ export class IncubationPeriod {
   static execute (agent) {
     const mensagem = 'O "período de incubação" significa o tempo entre a contração do vírus e o início dos sintomas da doença.\n' +
     '\n' +
-    '  Esse tempo varia de 1 a 14 dias, mas geralmente pode ocorrer em torno de 5 dias. \n' +
+    'Esse tempo varia de 1 a 14 dias, mas geralmente pode ocorrer em torno de 5 dias. \n' +
     '\n' +
     '⚠ No entanto, dados preliminares do Coronavírus sugerem que a transmissão possa ocorrer também mesmo sem o aparecimento de sinais e sintomas.'
 
