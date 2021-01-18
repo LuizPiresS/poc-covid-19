5c56c080ad4b12e3481d25763d9e6aa01aa5582f
Luiz Pires <luiz.pires@compasso.com.br>
2021-01-18 09:43:37 -0300

    refactor: movido o local das mensagens que serão mostradas para o usuario final

--- src/main/intents/cases/CasesInBrazilByStates.js
+++ src/main/intents/cases/CasesInBrazilByStates.js
@@ -7,7 +7,42 @@ export class CasesInBrazilByStates {
   static async execute (agent) {
     try {
       const apiCasesBrazil = new APICasesBrazil()
-      agent.add(new Text(await apiCasesBrazil.getCasesByStates(agent.parameters.states)))
+      const stateData = await apiCasesBrazil.getCasesByStates(agent.parameters.states)
+      if (stateData.statusCode === 500) {
+        agent.add(new Text('Desculpe, não estou conseguindo acessar o\n sistema no momento. Por favor, tente\n novamente mais tarde.'))
+        AnythingElse.helpMenu(agent)
+      }
+      const results = stateData.results[0]
+      const state = results.state
+      let messageSPRJ
+      if (state === 'SP') {
+        messageSPRJ = '👉 Para informações sobre a cidade de São Paulo busque por:\n ' +
+                      'São Paulo SP  👈'
+      } else if (state === 'RJ') {
+        messageSPRJ = '👉 Para informações sobre a cidade do Rio de Janeiro busque por:\n ' +
+                      'Rio de Janeiro RJ  👈'
+      } else {
+        messageSPRJ = ''
+      }
+      const message = `
+      Aqui estão os dados mais recentes para o estado ${state}:
+
+      ⚠ Total de casos:
+
+      - Confirmados: ${results.last_available_confirmed}
+      - Mortes: ${results.last_available_deaths}
+
+      ⚠ Casos no dia de hoje:
+
+      - Confirmados: ${results.new_confirmed}
+      - Mortes: ${results.new_deaths}
+
+       ${messageSPRJ}`
+
+      // const apiCasesBrazil = new APICasesBrazil()
+
+      agent.add(new Text(message))
+
       AnythingElse.helpMenu(agent)
     } catch (error) {
       console.log(error)
