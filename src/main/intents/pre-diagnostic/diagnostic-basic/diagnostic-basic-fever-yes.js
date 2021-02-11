d215509ed111b09dd903a18f4f2dbcc9df5b68b0
Luiz Pires <luiz.pires@compasso.com.br>
2021-02-11 09:44:07 -0300

    feat: adicionado reconhecimento de valores numéricos em febre

--- src/main/intents/pre-diagnostic/diagnostic-basic/diagnostic-basic-fever-yes.js
+++ src/main/intents/pre-diagnostic/diagnostic-basic/diagnostic-basic-fever-yes.js
@@ -1,9 +1,14 @@
 export class DiagnosticBasicFeverYes {
   static execute (agent) {
     const { groupOfRisk, threeOrMoreSymptoms, tookEffect, severeSymptoms } = agent.context.get('pre-diagnostic').parameters
-    agent.context.set({ name: 'pre-diagnostic', lifespan: 1, parameters: { groupOfRisk, fever: true, threeOrMoreSymptoms, tookEffect, severeSymptoms } })
 
-    console.log('yes------------------', agent.context.get('pre-diagnostic').parameters, '------------------ teve febre')
+    let fever = true
+    if (agent.context.get('fever-yes').parameters.number < 37.8) {
+      fever = false
+    }
+    agent.context.set({ name: 'pre-diagnostic', lifespan: 1, parameters: { groupOfRisk, fever, threeOrMoreSymptoms, tookEffect, severeSymptoms } })
+
+    console.log('teve febre ------------------', agent.context.get('fever-yes').parameters, '------------------ teve febre')
     agent.setFollowupEvent('pre-diagnostic-mild-symptoms-event')
   }
 }
